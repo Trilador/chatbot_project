2896e68b1d5967069b0e7d445fb67a66724c511d
Trilador <ttriladorr@gmail.com>
2023-09-27 21:09:59 -0400

    commit

--- chatbot_engine.py
+++ chatbot_engine.py
@@ -1,7 +1,24 @@
-import os
 import openai
 from utility_modules.file_operations import *
 from utility_modules.code_generations import *
+import os
+from typing import List, Tuple, Union, Dict, Any, Generator
+import openai
+from utility_modules.file_operations import (
+    list_files,
+    list_directories,
+    create_directory,
+    delete_directory,
+    display_file_content,
+    save_file_content,
+    search_file_content,
+)
+from utility_modules.code_generations import (
+    natural_language_to_code,
+    provide_code_feedback,
+    interactive_code_correction,
+    analyze_python_code_ast,
+)
 from utility_modules.web_operations import fetch_wikipedia_summary, search_python_documentation
 
 # Set up the OpenAI API
@@ -10,41 +27,19 @@ if not openai.api_key:
     raise ValueError("Please set the OPENAI_API_KEY environment variable.")
 
 DEFAULT_DIRECTORY = "C:\\Users\\timot\\Desktop\\Python\\AutoFix"
+TOKEN_LIMIT = 150
 
 # Contextual Understanding
-context = []
+context: List[str] = []
 
-def store_feedback(query, response, rating):
-    with open("feedback_data.txt", "a", encoding="utf-8") as f:  # Added encoding
+def store_feedback(query: str, response: str, rating: Union[int, str]) -> None:
+    with open("feedback_data.txt", "a", encoding="utf-8") as f:
         f.write(f"Query: {query}\n")
         f.write(f"Response: {response}\n")
         f.write(f"Rating: {rating}\n")
         f.write("-" * 50 + "\n")
 
-
-def natural_language_to_code(query):
-    # Basic templates for common tasks
-    templates = {
-        "read file": "with open('filename.txt', 'r') as file:\n    content = file.read()",
-        "write file": "with open('filename.txt', 'w') as file:\n    file.write('content')",
-        "basic loop": "for i in range(10):\n    print(i)"
-    }
-
-    return next(
-        (code for task, code in templates.items() if task in query),
-        "Can you provide more details or specify the task you want to perform?",
-    )
-
-def get_token_limit(query):
-    """Dynamically adjust the token limit based on the query's complexity."""
-    if len(query.split()) < 5:
-        return 50
-    elif len(query.split()) < 10:
-        return 100
-    else:
-        return 150
-
-def chatbot_response(query):  # sourcery skip: low-code-quality
+def chatbot_response(query: str) -> str:
     global context
     context.append(query)
     if len(context) > 5:  # Keep the last 5 interactions for context
@@ -69,11 +64,10 @@ def chatbot_response(query):  # sourcery skip: low-code-quality
         content = parts[-1].strip()
         return save_file_content(filename, content)
     elif "search content" in query:
-        parts = query.split("for")
-        content = parts[-1].strip()
+        content = query.split("for")[-1].strip()
         return search_file_content(content)
-    
-        # Code generation and analysis
+
+    # Code generation and analysis
     elif "generate code" in query:
         return natural_language_to_code(query)
     elif "provide feedback" in query:
@@ -86,34 +80,33 @@ def chatbot_response(query):  # sourcery skip: low-code-quality
     elif "analyze code" in query:
         code = query.split("for")[-1].strip()
         return analyze_python_code_ast(code)
-    
+
     # Wikipedia summary and web browsing
     elif "wikipedia summary" in query:
         return fetch_wikipedia_summary(query)
     elif "search documentation" in query:
         topic = query.split("for")[-1].strip()
         return search_python_documentation(topic)
-    
+
     else:
-        # Forward the query to OpenAI with context
         full_prompt = "\n".join(context + [query])
-        token_limit = get_token_limit(full_prompt)
-        response = openai.Completion.create(engine="davinci", prompt=full_prompt, max_tokens=token_limit)
-        response_text = response.choices[0].text.strip()
-    
+    response = openai.Completion.create(engine="davinci", prompt=full_prompt, max_tokens=TOKEN_LIMIT)
+    response_text = response.choices[0].text.strip()
+
     # Adjusted condition
-    if "My response seems too long." not in response_text and len(response_text.split()) > (token_limit // 2):
+    if len(response_text.split()) > TOKEN_LIMIT:
         return "My response seems too long. Would you like a more concise answer or should I clarify something specific?"
     return response_text
 
-def main():
+
+def main() -> None:
     while True:
         query = input("You: ")
         if query.lower() in ["exit", "quit", "bye"]:
             break
         response = chatbot_response(query)
         print(f"Bot: {response}")
-        
+
         # Feedback mechanism
         rating = input("Rate the response (1-5, 5 being very helpful, or 'skip' to skip): ")
         if rating.isdigit() and 1 <= int(rating) <= 5:
@@ -121,6 +114,9 @@ def main():
         elif rating.lower() != 'skip':
             print("Invalid rating. Skipping feedback for this response.")
 
+def get_token_limit() -> int:
+    return TOKEN_LIMIT
+
 if __name__ == "__main__":
     main()
-    
+
